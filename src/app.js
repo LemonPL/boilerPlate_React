7762adfcfb34e5fac783182df2b43fd83032a64e
Piotr Cytryniak <piotr.cytryniak@wonga.com>
2018-01-23 21:57:41 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss'
+
+// below we are rendering the whole app
+ReactDOM.render(<p> This is my boilerplate </p>, document.getElementById('app'));
+
+// there is normalize.css package running that normalizes .css layout on many browsers so the webpage looks the same
